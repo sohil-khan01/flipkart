2272528456712061fdba8f3b13d51c349bc71724
sohil-khan01 <spk612000@gmail.com>
2026-01-19 16:45:05 +0530

    bugg fixxx

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    proxy: {
+      '/api': {
+        // eslint-disable-next-line no-undef
+        target: process.env.VITE_API_URL || 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
   plugins: [react(), tailwindcss()],
 })
